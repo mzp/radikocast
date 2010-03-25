e02bcff34952a26c20e9c64cb538732eed5d7c2a
mzp <mzpppp@gmail.com>
2010-03-25 09:37:27 +0900

    schedulerのテストを書いた

---
+++ scheduler.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# -*- mode:python; coding:utf-8 -*-
+
+class Scheduler(object):
+    def add(self,**args):
+        pass
+
+    def invoke(self,time):
+        pass
--- scheduler_test.py
+++ scheduler_test.py
@@ -1,32 +1,38 @@
 #! /usr/bin/python
 # -*- mode:python; coding:utf-8 -*-
-
-
-from scheduler import *
 import time_table
-import datetime
-def time(hour,min):
-    return datetime.time(hour, min)
+from datetime import *
+from scheduler import *
+from nose.tools import *
 
 class TestScheduler:
     def setUp(self):
         def called(name):
-            def f(): self.called[name] = True
+            def f(): self.called[name] = self.called.get(name,0) + 1
             return f
         self.called = {}
         self.scheduler = Scheduler()
-        self.scheduler.add(time=time(12,10),at=time_table.Monday, agent=called('a'))
-        self.scheduler.add(time=time(13,20),at=time_table.Wednesday, agent=called('b'))
-        self.scheduler.add(time=time(12,10),at=time_table.Single, agent=called('c'))
+        self.scheduler.add(time=time(12,10),at=time_table.Monday,repeat=True, agent=called('a'))
+        self.scheduler.add(time=time(12,10),at=time_table.Monday,repeat=False, agent=called('b'))
+        self.scheduler.add(time=time(13,20),at=time_table.Wednesday,repeat=False, agent=called('c'))
 
     def test_invoke(self):
-        assert 1 == 1
+        self.scheduler.invoke(datetime(2010, 3, 1, 12, 10))
+        eq_(1, self.called.get('a',0))
+        eq_(1, self.called.get('b',0))
+        eq_(0, self.called.get('c',0))
 
     def test_repeat(self):
-        pass
+        self.scheduler.invoke(datetime(2010, 3, 1, 12, 10))
+        self.scheduler.invoke(datetime(2010, 3, 8, 12, 10))
+        eq_(2, self.called.get('a',0))
 
     def test_not_repeat(self):
-        pass
+        self.scheduler.invoke(datetime(2010, 3, 1, 12, 10))
+        self.scheduler.invoke(datetime(2010, 3, 8, 12, 10))
+        eq_(1, self.called.get('b',0))
 
     def test_ignore_sec(self):
-        pass
+        self.scheduler.invoke(datetime(2010, 3, 1, 12, 10, 30))
+        eq_(1, self.called.get('a',0))
+        eq_(1, self.called.get('b',0))
