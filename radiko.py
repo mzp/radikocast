8ae7eadf45cf86b1abe7663230f3fcb4e5f20e1b
mzp <mzpppp@gmail.com>
2010-03-26 13:54:46 +0900

    Revert "python2.4で動くようにした"
    This reverts commit c31206224bcc1e72bdd0f290288055e4924ad6b8.

--- radiko.py
+++ radiko.py
@@ -1,22 +1,9 @@
 #! /usr/bin/python
 # -*- mode:python; coding:utf-8 -*-
-#from uuid import uuid4
+from uuid import uuid4
 import os
 import os.path
 import logging
-
-import time, random, md5
-def uuid( *args ):
-    t = long( time.time() * 1000 )
-    r = long( random.random()*100000000000000000L )
-    try:
-        a = socket.gethostbyname( socket.gethostname() )
-    except:
-        # if we can't get a network address, just imagine one
-        a = random.random()*100000000000000000L
-    data = str(t)+' '+str(r)+' '+str(a)+' '+str(args)
-    data = md5.md5(data).hexdigest()
-    return data
 
 class Radiko(object):
     def __init__(self, storage, name, channel, **other):
@@ -28,7 +15,7 @@ class Radiko(object):
     def __call__(self, time, airtime):
         if not os.path.exists('audio'):
             os.makedirs('audio')
-        path = "audio/%s.aac" % uuid()
+        path = "audio/%s.aac" % uuid4().hex
         cmd = """
 rtmpdump -B %d -y "simul-stream" -n "radiko.smartstream.ne.jp" -c 1935
  -s "http://radiko.jp/player/player_0.0.9.swf"
