85e0328b4fcd2407e1bda7c3ad3ec28f84c01c99
mzp <mzpppp@gmail.com>
2010-04-24 16:08:36 +0900

    resume機能をちゃんと実装した

--- radiko.py
+++ radiko.py
@@ -15,21 +15,33 @@ class Radiko(object):
 rtmpdump --resume -B %d -y "simul-stream" -n "radiko.smartstream.ne.jp" -c 1935
  -s "http://radiko.jp/player/player_0.0.9.swf"
  -p "http://radiko.jp/player/player.html#QRR"
- -a "QRR/_defInst_" -f "WIN 10,0,45,2" -v -o -
- | ffmpeg -y -i - -acodec copy %s
+ -a "QRR/_defInst_" -f "WIN 10,0,45,2" -v -o - |
+ ffmpeg -y -i - -acodec copy %s
 """.replace("\n","")
+
+    def system(self,cmd):
+        logging.info(cmd)
+        os.system(cmd)
 
     def __call__(self, time, airtime):
         if not os.path.exists('audio'):
             os.makedirs('audio')
-        path = "audio/%s.aac" % uuid4().hex
+        uuid = uuid4().hex
+
+        # rec
         end = time + airtime
         now = datetime.now()
+        count = 0
         while now < end:
-            cmd = self.templ % ((end-now).seconds, path)
-            logging.info(cmd)
-            os.system(cmd)
+            path = "audio/%s-%d.aac" % (uuid, count)
+            self.system(self.templ % ((end-now).seconds, path))
             now = datetime.now()
+            count += 1
+
+        # concat
+        path = "audio/%s.aac" % uuid
+        self.system("cat audio/%s-*.aac > %s" % (uuid, path))
+
         self.storage.transaction(
             lambda : self.storage.add(name       = self.other['name'],
                                       original   = path,
